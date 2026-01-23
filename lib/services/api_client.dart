bf60e8d36b96737fc650dcf0c3cbb5e2423427b8
junior.matiello <junior.matiello@senior.com.br>
2026-01-23 15:12:05 -0300

    #AULA38 - Serviços parte 1

---
+++ lib/services/api_client.dart
@@ -0,0 +1,40 @@
+import 'package:desenvolvimento_flutter_iniciante/models/pessoa.dart';
+import 'package:dio/dio.dart';
+
+class ApiClient {
+  final Dio dio = Dio();
+
+  Future<List<Pessoa>> get() async{
+    final request = await dio.get("http://localhost:3000/pessoas");
+
+    if (request.statusCode == 200) {
+      final data = request.data;
+
+      final pessoasJson = data as List<Map<String, dynamic>>;
+
+      return pessoasJson
+        .map(
+          (pessoaJson) => Pessoa(
+            id: pessoaJson["id"],
+            nome: pessoaJson["nome"],
+            altura: pessoaJson["altura"],
+            peso: pessoaJson["peso"],
+          ),
+      )
+      .toList();
+    }
+    
+    throw Exception("Status Code invalido - Erro ao buscar pessoas");
+  }
+}
+
+
+Map<String, dynamic> pessoaJson = {
+  "id": "b345",
+  "nome": "Junior",
+  "altura": 165,
+  "peso": 64
+};
+
+final List<Map<String, dynamic>> lista = [pessoaJson];
+
